5aca4e6952058071c1a9b5301cdb209213142532
Jared Rosenberg <jrosenberg@rootinc.com>
2020-03-03 16:16:59 -0500

    tests

--- tests/FakerBonus/Provider/TweetTextTest.php
+++ tests/FakerBonus/Provider/TweetTextTest.php
@@ -11,14 +11,64 @@ class TweetTextTest extends TestCase
      */
     public function valid_standard_call()
     {
-        $tweet_text = $this->faker->tweetText;
-        $tweet_text_set = explode(' ', $tweet_text);
+        $tweet_text = $this->faker->tweetText();
+        $paragraph_set = preg_split('/[\r\n|\r|\n]+\s/', $tweet_text);
+        $text_set = explode(' ', $tweet_text);
 
-        $this->assertGreaterThan(1, count($tweet_text_set));
+        $this->assertGreaterThan(3, count($text_set));
+
+        $this->assertCount(1, $paragraph_set);
+
         // Has hashtag
         $this->assertRegExp('/\#[\w]{3,}/', $tweet_text);
+
         // Has mention
         $this->assertRegExp('/\@[\w]{3,}/', $tweet_text);
+
+        // Has emoji
+        $result = preg_match('/[\p{So}]/u', $tweet_text);
+        $this->assertTrue(!!$result);
     }
 
+    /**
+     * @test
+     */
+    public function valid_with_four_paragraphs()
+    {
+        $tweet_text = $this->faker->tweetText(4);
+        $paragraph_set = preg_split('/[\r\n|\r|\n]+\s/', $tweet_text);
+
+        $this->assertCount(4, $paragraph_set);
+
+        // Has hashtag
+        $this->assertRegExp('/\#[\w]{3,}/', $tweet_text);
+
+        // Has mention
+        $this->assertRegExp('/\@[\w]{3,}/', $tweet_text);
+
+        // Has emoji
+        $result = preg_match('/[\p{So}]/u', $tweet_text);
+        $this->assertTrue(!!$result);
+    }
+
+    /**
+     * @test Formatter is valid with "at" option = true (default)
+     */
+    public function valid_without_emoji()
+    {
+        $tweet_text = $this->faker->tweetText(1, false);
+        $paragraph_set = preg_split('/[\r\n|\r|\n]+\s/', $tweet_text);
+
+        $this->assertCount(1, $paragraph_set);
+
+        // Has hashtag
+        $this->assertRegExp('/\#[\w]{3,}/', $tweet_text);
+
+        // Has mention
+        $this->assertRegExp('/\@[\w]{3,}/', $tweet_text);
+
+        // No emoji
+        $result = preg_match('/[\p{So}]/u', $tweet_text);
+        $this->assertFalse(!!$result);
+    }
 }
